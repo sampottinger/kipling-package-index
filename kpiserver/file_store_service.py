e2b1d817374140ceb5ef65e48122ba0c265aa434
Sam Pottinger <samp@gleap.org>
2014-03-14 16:09:29 -0600

    Added some explanation.

--- kpiserver/file_store_service.py
+++ kpiserver/file_store_service.py
@@ -19,9 +19,18 @@ def create_file_upload_url(application, package_name):
     object_name = package_name + '.zip'
     mime_type = ZIP_MIME_TYPE
 
+    # Creating a signed URL where the user can post their file directly to S3,
+    # avoiding costs of transmission and request timeout limits. These signed
+    # URLs are strongly encouraged to have an expiration if POST.
     expires = int(time.time()+10)
+
+    # Amazon requires an access control level for each of its files. These
+    # uploads should be read only by the public.
     amz_headers = "x-amz-acl:public-read"
 
+    # We have to construct this by hand as opposed to using somebody like
+    # requestify because we have to hash it and sign it with our Amazon
+    # secret key.
     put_request = "PUT\n\n%s\n%d\n%s\n/%s/%s" % (
         mime_type,
         expires,
@@ -30,6 +39,7 @@ def create_file_upload_url(application, package_name):
         object_name
     )
 
+    # Signed with secret key without revealing that secret key to the world.
     signature = base64.encodestring(hmac.new(
         application.config['S3_SECRET_KEY'],
         put_request,
@@ -37,6 +47,7 @@ def create_file_upload_url(application, package_name):
     ).digest())
     signature = urllib.quote_plus(signature.strip())
 
+    # Build and return final URL
     url = 'https://%s.s3.amazonaws.com/%s' % (
         application.config['UPLOADS_BUCKET_NAME'],
         object_name
